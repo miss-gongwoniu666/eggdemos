f1d38aaaa43fa6bd2e6e7e948a7c63a765d29aa9
miss-gongwoniu666 <1216474433@qq.com>
2019-11-24 22:45:56 +0800

    中间件

--- config/config.default.js
+++ config/config.default.js
@@ -1,13 +1,23 @@
-exports.keys = 'gongjurui';
-// 添加 view 配置
-exports.view = {
-  defaultViewEngine: 'nunjucks',
-  mapping: {
-    '.tpl': 'nunjucks',
-  },
-};
 
-exports.news = {
-  pageSize: 5,
-  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+module.exports = {
+  keys: 'gongjurui',
+  // this.config.news
+  news: {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  },
+  // 配置： view模版插件
+  view : {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  },
+  // 配置 需要的中间件，数组顺序即为中间件的加载顺序
+  middleware: [ 'gzip' ],
+
+  // 配置 gzip 中间件的配置
+  gzip: {
+    threshold: 1024, // 小于 1k 的响应体不压缩
+  },
 };
